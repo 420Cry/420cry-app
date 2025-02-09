a1cd1fa668b02cdb6790440de89ea32b923a9f5c
TvTTiT <thanhtvfs0830@gmail.com>
2025-02-09 06:53:38 +0100

    update i18n

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,9 +3,10 @@ import { cookies, headers } from 'next/headers'
 
 export default getRequestConfig(async () => {
   const localeFromHeaders =
-    (await headers()).get('accept-language')?.split(',')[0] || 'en'
+    (await headers()).get('accept-language')?.split(',')[0] || 'en'  
+  // Check if the locale is supported (en or vi), otherwise fall back to en
   const userLocale = (await cookies()).get('locale')?.value || localeFromHeaders
-  const baseLocale = userLocale.split('-')[0]
+  const baseLocale = ['en', 'vi'].includes(userLocale.split('-')[0]) ? userLocale.split('-')[0] : 'en'
   const messages = (await import(`../../messages/${baseLocale}.json`)).default
 
   return {
